5fb2bc399e04bc980b9cb6ba9b070a0981fc8882
Vladan Ristic <vladanrstc@gmail.com>
2022-09-24 15:52:56 +0200

    added sections as module routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Route;
 class RouteServiceProvider extends ServiceProvider
 {
 
-    private $moduleList = ["Auth", "Course", "User", "Stats", "Notes", "Reviews", "CourseStart"];
+    private $moduleList = ["Auth", "Course", "User", "Stats", "Notes", "Reviews", "CourseStart", "Sections"];
 
     /**
      * The path to the "home" route for your application.
