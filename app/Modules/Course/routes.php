e52337968d4282d747adca7bc879b80d30ddb3c7
Vladan Ristic <vladanrstc@gmail.com>
2022-10-09 23:24:33 +0200

    made all courses be public

--- app/Modules/Course/routes.php
+++ app/Modules/Course/routes.php
@@ -4,11 +4,7 @@ use App\Modules\Course\Controllers\CourseController;
 use Illuminate\Support\Facades\Route;
 
 Route::get("/all", [CourseController::class, "allCourses"]);
-
-Route::group(['middleware' => ['auth:api', 'scope:user']], function () {
-    // course details
-    Route::get("/details/{course}", [CourseController::class, "courseDetails"]);
-});
+Route::get("/details/{course}", [CourseController::class, "courseDetails"]);
 
 Route::group(['middleware' => ['scope:admin,super-admin']], function () {
     Route::resource('courses', CourseController::class);
--- app/Modules/Reviews/routes.php
+++ app/Modules/Reviews/routes.php
@@ -3,9 +3,10 @@
 use App\Modules\Reviews\Controllers\ReviewController;
 use Illuminate\Support\Facades\Route;
 
+Route::get("/course/{courseSlug}", [ReviewController::class, "getCourseReviews"]);
+
  Route::group(['middleware' => ['auth:api','scope:user,admin,super-admin']], function () {
     Route::patch("/courses/started/review", [ReviewController::class, "updateCourseReview"]);
-    Route::get("/course/{courseSlug}", [ReviewController::class, "getCourseReviews"]);
     Route::get("/reviews/course/user/{course}", [ReviewController::class, "course_reviews_user"]);
 });
 
