2bb7ad0594c4d5f16ca0b1f30a14607fbec0a8bb
Vladan Ristic <vladanrstc@gmail.com>
2022-10-10 22:05:35 +0200

    added course non auth route

--- app/Modules/Course/routes.php
+++ app/Modules/Course/routes.php
@@ -6,6 +6,10 @@ use Illuminate\Support\Facades\Route;
 Route::get("/all", [CourseController::class, "allCourses"]);
 Route::get("/details/{course}", [CourseController::class, "courseDetails"]);
 
+Route::group(['middleware' => ['auth:api', 'scope:user']], function () {
+    Route::get("/user-details/{course}", [CourseController::class, "courseDetails"]);
+});
+
 Route::group(['middleware' => ['scope:admin,super-admin']], function () {
     Route::resource('courses', CourseController::class);
     Route::post("/courses/update/{course}", [CourseController::class, "update"]);
