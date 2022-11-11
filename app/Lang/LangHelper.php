2d217677730a562b17c9f076767205357a53a484
Vladan Ristic <vladanrstc@gmail.com>
2022-11-11 14:50:48 +0100

    fixed app dir not being found

--- app/Lang/LangHelper.php
+++ app/Lang/LangHelper.php
@@ -25,7 +25,7 @@ class LangHelper implements ILangHelper
         $messages = json_decode(
             file_get_contents(
                 base_path(
-                    "App" . DIRECTORY_SEPARATOR . "Modules" . DIRECTORY_SEPARATOR . $module->value . DIRECTORY_SEPARATOR . "Messages" . DIRECTORY_SEPARATOR . $language . ".json")),
+                    "app" . DIRECTORY_SEPARATOR . "Modules" . DIRECTORY_SEPARATOR . $module->value . DIRECTORY_SEPARATOR . "Messages" . DIRECTORY_SEPARATOR . $language . ".json")),
             true);
 
         if (isset($messages[$key])) {
